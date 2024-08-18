7c93b5e1f3c9b8c10f04fedf69c8606e4b433956
RAX3R <45417916+vqpv@users.noreply.github.com>
2024-08-18 09:31:39 +0300

    2.1 - 1

---
+++ 2. Знакомство с функциями/2.1 Что такое функция. Определение функции/1.py
@@ -0,0 +1,10 @@
+def say_hello(name):
+    print(f"Привет, {name}!")
+
+
+def choose_profession(profession):
+    print(f'Я хочу стать {profession}ом')
+
+
+say_hello("Артем")
+choose_profession("Разработчик")
