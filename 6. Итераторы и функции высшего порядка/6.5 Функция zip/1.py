5048330f7a94cfe8e5b8c303bf05151eedd0e961
RAX3R <45417916+vqpv@users.noreply.github.com>
2024-12-16 23:59:47 +0300

    6.5 - 1

---
+++ 6. Итераторы и функции высшего порядка/6.5 Функция zip/1.py
@@ -0,0 +1,6 @@
+keys = ['Ten', 'Twenty', 'Thirty', 'Forty', 'Fifty', 'Sixty', 'Seventy', 'Eighty', 'Ninety', 'One hundred']
+values = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
+
+d = dict(zip(keys, values))
+
+print(d)
