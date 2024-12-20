60f6d6575f3821bf94394554989dd391e8525ea9
RAX3R <45417916+vqpv@users.noreply.github.com>
2024-12-20 19:05:36 +0300

    6.6 - 1

---
+++ 6. Итераторы и функции высшего порядка/6.6 Функция enumerate/1.py
@@ -0,0 +1,7 @@
+english_words = ('attack', 'bless', 'look', 'reckless', 'short', 'monster', 'trolley', 'sound',
+                 'ambiguity', 'researcher', 'trunk', 'coat', 'quantity', 'question', 'tenant',
+                 'miner', 'definite', 'kit', 'spectrum', 'satisfied', 'selection', 'carve',
+                 'ask', 'go', 'suggest')
+
+for number, word in enumerate(english_words):
+    print(f"Word № {number + 1} = {word}")
