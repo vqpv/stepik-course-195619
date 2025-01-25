903bb7f17f3bcffdaff467c106f9cb2861b10d70
RAX3R <45417916+vqpv@users.noreply.github.com>
2025-01-25 22:44:06 +0300

    8.2 - 2

---
+++ 8. Генераторы/8.2 Выражения-генераторы/2.py
@@ -0,0 +1,7 @@
+words = ['feel', 'graduate', 'movie', 'fashionable', 'bacon', 'drop', 'produce', 'acquisition',
+         'cheap', 'strength', 'master', 'perception', 'noise', 'strange', 'am']
+
+lens = (len(i) for i in words)
+
+for i in lens:
+    print(i)
